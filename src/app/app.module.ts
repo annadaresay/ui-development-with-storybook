3d651eba2ce253d3253d062a1c6504b251410c1c
Anna Viklund <anna.viklund@daresay.co>
2022-01-18 13:55:05 +0100

    Move and tweak button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import ButtonComponent from '../stories/Button/button.component';
+import ButtonComponent from './components/button/button.component';
 
 @NgModule({
   declarations: [AppComponent, ButtonComponent],
--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'storybook-button',
+  selector: 'app-button',
   template: ` <button
     type="button"
     (click)="onClick.emit($event)"
@@ -46,10 +46,8 @@ export default class ButtonComponent {
   onClick = new EventEmitter<Event>();
 
   public get classes(): string[] {
-    const mode = this.primary
-      ? 'storybook-button--primary'
-      : 'storybook-button--secondary';
+    const mode = this.primary ? 'app-button--primary' : 'app-button--secondary';
 
-    return ['storybook-button', `storybook-button--${this.size}`, mode];
+    return ['app-button', `app-button--${this.size}`, mode];
   }
 }
