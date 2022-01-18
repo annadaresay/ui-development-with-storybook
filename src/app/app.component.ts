1b3a3fdb0070fdfbeb97095c616ec58a62ee5ba9
Anna Viklund <anna.viklund@daresay.co>
2022-01-18 13:32:32 +0100

    Setup example page + cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'intro-storybook-angular-template';
+
+  onClick(_event: unknown) {
+    alert('Button was clicked!');
+  }
 }
--- src/stories/Button/Button.stories.ts
+++ src/stories/Button/Button.stories.ts
@@ -1,9 +1,8 @@
-// also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/angular/types-6-0';
+import { Story, Meta } from '@storybook/angular';
 import Button from './button.component';
 
 export default {
-  title: 'Example/Button',
+  title: 'UIKit/Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
